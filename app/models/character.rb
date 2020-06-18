1f19c192b773d140aaf55c726924cea86f417f9f
Tisdale511 <tisdalefry@gmail.com>
2020-06-18 12:43:02 -0700

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def build_network
+        char = Network.new(:name => "name")
+        # char.build_show(:name => "name").build_network(:call_letters => "call_letters")
+        binding.pry
+    end
+
+    def say_that_thing_you_say
+       "#{self.name} always says: #{self.catchphrase}" 
+    end
 end
